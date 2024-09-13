5de926775786e515da3c25fcc86fabf63082ef75
broosbros <veerhasbeenfound@gmail.com>
2024-09-13 13:46:21 +0530

    Add files via upload

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,26 +1,33 @@
 import pandas as pd
 import os
 from tqdm import tqdm
-from utils import download_images  
+from utils import download_images
+import random
 
-def download_dataset_images(csv_path, download_folder):
+def download_dataset_images(csv_path, download_folder, num_images=25000):
     df = pd.read_csv(csv_path)
     
     image_links = df['image_link'].unique()
-    
-    print(f"Found {len(image_links)} unique images to download.")
-    
+
+    if len(image_links) < num_images:
+        print(f"Only {len(image_links)} unique images found, downloading all.")
+        num_images = len(image_links)
+
+    sampled_image_links = random.sample(list(image_links), num_images)
+
+    print(f"Found {len(image_links)} unique images, downloading {num_images} random images.")
+
     os.makedirs(download_folder, exist_ok=True)
-    
+
     print("Downloading images...")
-    download_images(image_links, download_folder, allow_multiprocessing=True)
-    
+    download_images(sampled_image_links, download_folder, allow_multiprocessing=True)
+
     print("Download complete!")
-    
+
     downloaded_images = os.listdir(download_folder)
     print(f"Successfully downloaded {len(downloaded_images)} images.")
-    
-    missing_images = set(image_links) - set(downloaded_images)
+
+    missing_images = set(sampled_image_links) - set(downloaded_images)
     if missing_images:
         print(f"Warning: {len(missing_images)} images could not be downloaded.")
         print("First few missing image links:")
@@ -28,12 +35,12 @@ def download_dataset_images(csv_path, download_folder):
             print(link)
 
 
-train_csv_path = '../dataset/train.csv'  
-train_download_folder = 'images/train/' 
+train_csv_path = '../dataset/train.csv'
+train_download_folder = 'images/train/'
 
-download_dataset_images(train_csv_path, train_download_folder)
+download_dataset_images(train_csv_path, train_download_folder, num_images=25000)
 
-test_csv_path = '../dataset/test.csv'  
-test_download_folder = 'images/test/' 
+test_csv_path = '../dataset/test.csv'
+test_download_folder = 'images/test/'
 
-download_dataset_images(test_csv_path, test_download_folder)
+download_dataset_images(test_csv_path, test_download_folder, num_images=10000)
